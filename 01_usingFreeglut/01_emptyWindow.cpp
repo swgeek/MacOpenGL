520e3e764aa1f39f544fa39d01bb3c9039d4b21b
Manoj Patel <sw@mpvisual.com>
2014-06-26 23:18:38 -0700

    Empty Window again, but using freeglut
    The build in glut only supports older versions, so also going to
    provide versions using freeglut

---
+++ 01_usingFreeglut/01_emptyWindow.cpp
@@ -0,0 +1,22 @@
+
+// complile using: g++ 01_emptyWindow.cpp -o 01_emptyWindow -framework OpenGL -lGLUT
+
+#include <GL/freeglut.h>
+
+void render(void)
+{
+}
+
+int main(int argc, char ** argv)
+{
+   glutInit(&argc, argv);
+   glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+   glutInitWindowPosition(100, 100);
+   glutInitWindowSize(500, 500);
+   glutCreateWindow("Simple Window");
+   glutDisplayFunc(render);
+   glutMainLoop();
+
+   return 1;
+}
+
